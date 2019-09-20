742191461d4d40cd6e7cdb556d3d74ebe7650d67
Thanatos91 <fhachemi@lerebours.org>
2019-09-20 08:33:42 +0200

    Ajoutez des fichiers projet.

---
+++ ModelObjet/Condition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ModelObjet
+{
+    public class Condition
+    {
+        const int nbJours = 30;
+        // Permet de savoir si on a le droit d'être remboursé
+        // On l'est à condition que le nombre de jours ne dépasse pas 30 !
+        public static bool Valider(int unNbDeJours)
+        {
+            if (unNbDeJours < 30)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        // Permet de renvoyer le montant MAX remboursé en fonction de la catégorie
+        public static int CalculerMontantMax(string uneCategorie)
+        {
+            if (uneCategorie == "Livre")
+            {
+                return 30;
+            }
+            if (uneCategorie == "Jouet")
+            {
+                return 50;
+            }
+            if (uneCategorie == "Informatique")
+            {
+                return 1000;
+            }
+        }
+        // Permet de retourner le total remboursé en fonction de tous les critères
+        public static double CalculerMontantRembourse(int unNbDeJours, string uneCategorie, bool estMembre, string unEtat, int unPrix)
+        {
+            return 0;
+
+        }
+        // Permet de renvoyer la réduction si on est membre ou pas
+        public static double CalculerReductionMembre(bool estMembre)
+        {
+            return 0;
+        }
+        // Permet de renvoyer la réduction en fonction de l'état de l'achat
+        public static double CalculerReduction(string unEtat)
+        {
+            return 0;
+        }
+    }
+}
---
+++ ProjetDeTests/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelObjet;
+
+namespace ProjetDeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void ValiderTest()
+        {
+            // Le nombre de jours d'achat est < à 30 jours
+            const int nbJours1 = 25;
+            Assert.AreEqual(true, Condition.Valider(nbJours1));
+
+            // Le nombre de jours d'achat est > à 30 jours
+            const int nbJours2 = 35;
+            Assert.AreEqual(false, Condition.Valider(nbJours2));
+        }
+
+        [TestMethod()]
+        public void CalculerMontantMaxTest()
+        {
+            // Pour la catégorie livre
+            
+
+            // Pour la catégorie jouet
+            
+
+            // Pour la catégorie informatique
+            
+        }
+
+        [TestMethod()]
+        public void CalculerMontantRembourseTest()
+        {
+            // Un livre achété 24 euros depuis 15 jours avec un état "Très abimé" en étant non membre
+
+            // Un livre achété 24 euros depuis 15 jours avec un état "Bon" en étant membre
+
+        }
+
+        [TestMethod()]
+        public void CalculerReductionMembreTest()
+        {
+            // Il est membre
+            
+
+
+            // Il n'est pas membre
+            
+
+        }
+
+        [TestMethod()]
+        public void CalculerReductionTest()
+        {
+            // Pour un état "bon"
+            
+
+
+            // Pour un état "abimé"
+            
+
+        }
+    }
+}
